96ebe1d41d5eb6cd8dd486241dc1a74da9824fc1
akashsharma99 <akash.asharma.sharma11@gmail.com>
2021-06-28 19:17:05 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const CACHE_VERSION = 'v5';
 const cacheItems = [
-    '/styles.css',
-    '/index.html',
-    '/app.js',
-    '/uselessnotepad.webmanifest',
-    '/assets/app-icon-192.png',
-    '/assets/app-icon-512.png',
-    '/assets/favicon.svg',
-    '/assets/dark-icon.svg',
-    '/assets/light-icon.svg',
-    '/assets/moon.svg',
-    '/assets/sun.svg',
+    '/Useless-Notepad/styles.css',
+    '/Useless-Notepad/index.html',
+    '/Useless-Notepad/app.js',
+    '/Useless-Notepad/uselessnotepad.webmanifest',
+    '/Useless-Notepad/assets/app-icon-192.png',
+    '/Useless-Notepad/assets/app-icon-512.png',
+    '/Useless-Notepad/assets/favicon.svg',
+    '/Useless-Notepad/assets/dark-icon.svg',
+    '/Useless-Notepad/assets/light-icon.svg',
+    '/Useless-Notepad/assets/moon.svg',
+    '/Useless-Notepad/assets/sun.svg',
 ]
 self.addEventListener('install', (event) => {
     event.waitUntil(
